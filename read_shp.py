0cda86e0c2e7b365783884bde142f5d9d054944f
shogonir <shogonir@gmail.com>
2016-08-08 01:39:38 +0900

    [modify] enable output map image

--- read_shp.py
+++ read_shp.py
@@ -3,6 +3,9 @@
 
 import os
 import struct
+import pickle
+
+from PIL import Image
 
 
 class BinaryReader :
@@ -34,12 +37,32 @@ class BinaryReader :
         hex_string = struct.pack('BBBB', *byte_list)
         return struct.unpack('i', hex_string)[0]
 
+    def readShortAsBig (self) :
+        result = 0
+        for i in range(2) :
+            result *= 2 ** 8
+            result += self.readByteAsNumber()
+        return result
+
+    def readShortAsLittle (self) :
+        byte_list = []
+        for i in range(2) :
+            byte_list.append(self.readByteAsNumber())
+        hex_string = struct.pack('BB', *byte_list)
+        return struct.unpack('H', hex_string)[0]
+
     def readDoubleAsLittle (self) :
         byte_list = []
         for i in range(8) :
             byte_list.append(self.readByteAsNumber())
         hex_string = struct.pack('BBBBBBBB', *byte_list)
         return struct.unpack('d', hex_string)[0]
+
+    def readString (self, num_characters) :
+        byte_list = []
+        for i in range(num_characters) :
+            byte_list.append(self.readByteAsNumber())
+        return ''.join(map(chr, byte_list))
 
 
 class ShapeMain :
@@ -103,8 +126,8 @@ class ShapeMainHeader :
         print '    Version     : {0:6d}'.format(self.version)
         print '    Shape Type  : {0:6d}'.format(self.shape_type)
         print '    Xmin        : {0:f}'.format(self.xmin)
-        print '    Ymin        : {0:f}'.format(self.ymin)
         print '    Xmax        : {0:f}'.format(self.xmax)
+        print '    Ymin        : {0:f}'.format(self.ymin)
         print '    Ymax        : {0:f}'.format(self.ymax)
 
 
@@ -281,6 +304,75 @@ class Point :
         self.length = self.LENGTH
 
 
+class Vector2 :
+    
+    def __init__ (self, x, y) :
+        self.x = x
+        self.y = y
+
+
+class PngCreator :
+    
+    @staticmethod
+    def createFromShapeMains (shapes) :
+        ratio = 40000
+        xmin, xmax, ymin, ymax = PngCreator.extractMinMax(shapes)
+        width  = int(ratio * (xmax - xmin)) + 1
+        height = int(ratio * (ymax - ymin)) + 1
+        image  = Image.new('RGB', (width, height), (0x00, 0x00, 0x00))
+        for shape in shapes :
+            approved_types = [ShapeMainRecord.SHAPE_TYPE_POLY_LINE, ShapeMainRecord.SHAPE_TYPE_POLYGON]
+            if shape.header.shape_type in approved_types :
+                for record in shape.records :
+                    for pointno, point in enumerate(record.content.points) :
+                        if pointno != 0 :
+                            point1 = PngCreator.normalizePoint(pre_point, xmin, xmax, ymin, ymax, width, height)
+                            point2 = PngCreator.normalizePoint(point,     xmin, xmax, ymin, ymax, width, height)
+                            line = PngCreator.drawLine(point1, point2)
+                            for p in line :
+                                if 0 <= p.x and p.x < width and 0 <= p.y and p.y < height :
+                                    image.putpixel((p.x, p.y), (0xFF, 0xFF, 0xFF))
+                        pre_point = point
+        image.show()
+
+    @staticmethod
+    def normalizePoint (point, xmin, xmax, ymin, ymax, width, height) :
+        x = int(1.0 * width  * (point.x - xmin) / (xmax - xmin))
+        y = int(1.0 * height * (point.y - ymin) / (ymax - ymin))
+        return Vector2(x, y)
+
+    @staticmethod
+    def extractMinMax (shapes) :
+        h0 = shapes[0].header
+        xmin, xmax, ymin, ymax = h0.xmin, h0.xmax, h0.ymin, h0.ymax
+        for shape in shapes :
+            xmin = shape.header.xmin if shape.header.xmin < xmin else xmin
+            xmax = shape.header.xmax if shape.header.xmax > xmax else xmax
+            ymin = shape.header.ymin if shape.header.ymin < ymin else ymin
+            ymax = shape.header.ymax if shape.header.ymax > ymax else ymax
+        return xmin, xmax, ymin, ymax
+
+    @staticmethod
+    def drawLine (point1, point2) :
+        points = []
+        try :
+            slope = 1.0 * (point2.y - point1.y) / (point2.x - point1.x)
+            if abs(slope) > 1.0 :
+                top, bottom = (point1, point2) if point1.y > point2.y else (point2, point1)
+                for y in range(bottom.y, top.y + 1) :
+                    progress = 1.0 * (y - bottom.y) / (top.y - bottom.y)
+                    x = int(bottom.x + progress * (top.x - bottom.x))
+                    points.append(Vector2(x, y))
+            else :
+                right, left = (point1, point2) if point1.x > point2.x else (point2, point1)
+                for x in range(left.x, right.x + 1) :
+                    progress = 1.0 * (x - left.x) / (right.x - left.x)
+                    y = int(left.y + progress * (right.y - left.y))
+                    points.append(Vector2(x, y))
+        except ZeroDivisionError :
+            pass
+        return points
+
 if __name__ == '__main__' :
     
     dname = '544022'
@@ -291,8 +383,7 @@ if __name__ == '__main__' :
         extension = fname.split('.')[1]
         exec('{0:s}s.append(dname + "/" + fname)'.format(extension))
     
-    for shp_name in shps :
-        shp_file = ShapeMain(shp_name)
+    shapes = [ShapeMain(fname) for fname in shps]
 
-    for shx_name in shxs :
-        shx_file = ShapeIndex(shx_name)
+    PngCreator.createFromShapeMains(shapes)
+
